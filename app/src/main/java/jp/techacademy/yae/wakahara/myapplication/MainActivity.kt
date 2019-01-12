aecde44100766da0d39e9e172dd3df0e4abb4525
yae-wak <yae_suzu@hotmail.com>
2019-01-12 14:33:26 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/yae/wakahara/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.yae.wakahara.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
